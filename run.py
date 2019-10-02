1a9f5a232d53d2f8a89e426d00af975dc6dfab28
qmShen <joyshen06@gmail.com>
2019-10-02 13:02:10 +0800

    save

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import time
 
 # The port number should be the same as the front end
 # try:
-app.run(use_reloader=True, debug=True, port = 9950)
+app.run(host='127.0.0.1', port = 9950, use_reloader=False, debug=True)
 # except:
 #     print("Some thing wrong!")
 
